884ec67707a2b5d0849d1d2ff7c3c0d58e10e7ff
sergio <sergio_e_f_capela@outlook.pt>
2023-10-12 10:57:25 +0100

    home page

--- app/src/main/java/com/example/gomoku/authors/AuthorView.kt
+++ app/src/main/java/com/example/gomoku/authors/AuthorView.kt
@@ -2,11 +2,16 @@ package com.example.gomoku.authors
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun AuthorScreen() {
@@ -15,8 +20,18 @@ fun AuthorScreen() {
         verticalArrangement = Arrangement.Center
     ) {
         Text(fontFamily = FontFamily.Serif, text = "Vasco Branco, number -> 48259")
+        Spacer(modifier = Modifier.width(10.dp))
         Text(fontFamily = FontFamily.Serif, text = "José Borges, number -> 48269")
+        Spacer(modifier = Modifier.width(10.dp))
         Text(fontFamily = FontFamily.Serif, text = "Sérgio Capela, number -> 46080")
+        Spacer(modifier = Modifier.width(10.dp))
         Button(onClick = { }) { Text(text = "Send a message to support the creators") }
     }
 }
+
+@Preview(showSystemUi = true)
+@Composable
+fun AuthorScreenPrev(){
+    AuthorScreen()
+}
+
--- app/src/main/java/com/example/gomoku/home/HomeView.kt
+++ app/src/main/java/com/example/gomoku/home/HomeView.kt
@@ -20,6 +20,7 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.gomoku.R
 import com.example.gomoku.ui.theme.GomokuTheme
 
@@ -41,6 +42,7 @@ fun HomeScreen() {
                 Text(
                     fontFamily = FontFamily.Serif,
                     text = "Gomoku",
+                    fontSize = 50.sp,
                     fontWeight = FontWeight.Bold,
                     textAlign = TextAlign.Center,
                     modifier = Modifier.fillMaxWidth()
