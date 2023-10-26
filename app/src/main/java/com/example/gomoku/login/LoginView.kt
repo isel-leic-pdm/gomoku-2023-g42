8e3803a6848886b59fd1bed7eb74091d0c234296
sergio <sergio_e_f_capela@outlook.pt>
2023-10-26 09:06:01 +0100

    Login

--- app/src/main/java/com/example/gomoku/login/LoginView.kt
+++ app/src/main/java/com/example/gomoku/login/LoginView.kt
@@ -1,4 +1,4 @@
-package com.example.gomoku.home
+package com.example.gomoku.login
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
@@ -10,13 +10,20 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -24,8 +31,13 @@ import androidx.compose.ui.unit.sp
 import com.example.gomoku.R
 import com.example.gomoku.ui.theme.GomokuTheme
 
+
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun HomeView() {
+fun LoginView() {
+    var username = remember{mutableStateOf("")}
+    val password = remember{mutableStateOf("")}
+
     GomokuTheme {
         Column(
             modifier = Modifier.fillMaxSize(),
@@ -50,21 +62,31 @@ fun HomeView() {
                     painter = painterResource(id = R.drawable.logo),
                     contentDescription = null
                 )
+                OutlinedTextField(
+                    value = username.value,
+                    onValueChange = {username.value = it},
+                    label = { Text(text = "Username")},
+                )
+                Spacer(modifier = Modifier.width(30.dp))
+                OutlinedTextField(
+                    value = password.value,
+                    onValueChange = {password.value = it},
+                    label = { Text(text = "Password")},
+                    visualTransformation = PasswordVisualTransformation()
+                )
+                Spacer(modifier = Modifier.width(30.dp))
+                Text(text = "Sign in")
             }
-            Row(
-                modifier = Modifier.fillMaxWidth(),
-                horizontalArrangement = Arrangement.Center
-            ) {
-                Button(onClick = { }) { Text(text = "Play") }
-                Spacer(modifier = Modifier.width(10.dp))
-                Button(onClick = { }) { Text(text = "Authors") }
-            }
+
         }
     }
+
 }
 
 @Preview(showSystemUi = true)
 @Composable
-fun HomeViewPreview(){
-    HomeView()
+fun LoginViewPreview(){
+    LoginView()
 }
+
+
